d513f124506d26e8e3a8a1e1bae3155dc6dd13d4
Reno <erickjtorres03@gmail.com>
2026-06-16 14:56:20 -0400

    phase 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,26 @@ import './App.css'
 export default function App() {
 
   const [name, setName] = useState('')
-  const [email, setEmail] = useState('') //start of phase 2
+  const [email, setEmail] = useState('') 
 
   return (
     <div>
-      <h1>RSVP Practice</h1>
-      <input type="text" value = {name} onChange = { (event) => {
-        //we set the value to the name variable since name's value is a string
-        // then with the onChange function we make it target the input 
-        // with event.target and since we want the string in the event target (the input box)
-        // we use setName to change the value of name to whatever is in the input box
-        setName(event.target.value)
-      } } />
-      {/*  Then here we are just calling the name variable again with the updated value due to onChange */}
-      <p>What you are typing : {name}</p>
+      <h1> Company Picnic RSVP </h1>
+      <form>
+        <label>Name :  <input type="text" value={name} onChange = {(e)=>{
+            setName(e.target.value)
+        }} />  
+        </label>
+        <br />
+        <label>Email : <input type="text" value={email} onChange = {(v)=>{
+            setEmail(v.target.value)
+        }} />  
+        </label>
+        <br/>
+        <button type="submit"><label>Add Guest</label></button>
+      </form>
+      <p>{name}</p>
+      <p>{email}</p>
     </div>
   )
 }
