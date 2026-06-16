89c5552c659287d524388cabe3fe65e5b60fe989
Reno <erickjtorres03@gmail.com>
2026-06-16 14:59:26 -0400

    part 2 complete heres comments on changes from part 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
 
   const [name, setName] = useState('')
   const [email, setEmail] = useState('') 
-
+// Per the request of the direction we made 2 state variables
   return (
     <div>
       <h1> Company Picnic RSVP </h1>
@@ -13,7 +13,7 @@ export default function App() {
         <label>Name :  <input type="text" value={name} onChange = {(e)=>{
             setName(e.target.value)
         }} />  
-        </label>
+        </label> {/* here we re use the input onChange we did in part 1 to better suit the web application */}
         <br />
         <label>Email : <input type="text" value={email} onChange = {(v)=>{
             setEmail(v.target.value)
@@ -22,8 +22,9 @@ export default function App() {
         <br/>
         <button type="submit"><label>Add Guest</label></button>
       </form>
-      <p>{name}</p>
+      <p>{name}</p> {/* Here we call the state variables back to see real time changes on the website as we put strings in the user input manually */}
       <p>{email}</p>
     </div>
   )
 }
+// part 2 complete
